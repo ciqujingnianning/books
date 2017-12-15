2a110314a8d729c69b05bcd4213797ca4e022262
gaoning <1337145936@qq.com>
2017-12-15 20:29:25 +0800

    再次提交

--- src/main/java/com/book/controller/BookDelAction.java
+++ src/main/java/com/book/controller/BookDelAction.java
@@ -23,7 +23,7 @@ public class BookDelAction extends ActionSupport{
     private static final long serialVersionUID = 1L;
     private int id;
     public int getId() {
-        return id;
+       return id;
     }
     public void setId(int id) {
         this.id = id;
